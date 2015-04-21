56a4147bed6ea3a62c6d324ecc44bfe7364a7f33
Yifan Xiao <xiaoyifan@uchicago.edu>
2015-04-21 16:23:27 -0500

    finished Core Data saving and data fetching, installed Parse sdk.

--- musicStore/AppDelegate.swift
+++ musicStore/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("0Ljy0kCOd8O5lG1AucTADd1UhFqqUcuYInVNA6sz",
+            clientKey: "FssyOOl5cl3fGefKWicN5X618juTqlkEMp67ll2S")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
--- musicStore/ViewController.swift
+++ musicStore/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import musicStoreKit
+import CoreData
 
 class ViewController: UIViewController {
     
@@ -16,6 +17,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var appDel:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        var context:NSManagedObjectContext = appDel.managedObjectContext!
+        
+        clearData("App", context: context)
         
         var request = NSMutableURLRequest(URL: NSURL(string: "https://itunes.apple.com/search?term=music&country=us&entity=software")!)
         Networking.sharedInstance.get(request){
@@ -26,8 +31,87 @@ class ViewController: UIViewController {
                 //println(data)
                 self.objects = dataParsing.sharedInstance.parse(data)
                 
+                var appDel:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+                var context:NSManagedObjectContext = appDel.managedObjectContext!
+                
+                for user in self.objects as! [appItem]{
+                    
+                    var newApp: AnyObject = NSEntityDescription.insertNewObjectForEntityForName("App", inManagedObjectContext: context)
+                    
+                    newApp.setValue(user.screenshotUrls, forKey: "screenshotUrls")
+                    newApp.setValue(user.artworkUrl60, forKey: "artworkUrl60")
+                    newApp.setValue(user.ipadScreenshotUrls, forKey: "ipadScreenshotUrls")
+                    newApp.setValue(user.features, forKey: "features")
+                    newApp.setValue(user.supportedDevices, forKey: "supportedDevices")
+                    newApp.setValue(user.trackCensoredName, forKey: "trackCensoredName")
+                    newApp.setValue(user.languageCodesISO2A, forKey: "languageCodesISO2A")
+                    newApp.setValue(user.contentAdvisoryRating, forKey: "contentAdvisoryRating")
+                    newApp.setValue(user.trackViewUrl, forKey: "trackViewUrl")
+                    newApp.setValue(user.currency, forKey: "currency")
+                    newApp.setValue(user.price, forKey: "price")
+                    newApp.setValue(user.version, forKey: "version")
+                    newApp.setValue(user.objDescription, forKey: "objDescription")
+                    newApp.setValue(user.minimumOsVersion, forKey: "minimumOsVersion")
+                    
+                    println(newApp)
+                    
+                }
+                
+                var err:NSError?
+                if !context.save(&err){
+                    println(err)
+                }
+                else{
+                    println("saving succeeded")
+                }
+                
             }
         }
+    }
+    
+    func clearData(entity:String, context: NSManagedObjectContext){
+        
+        var request = NSFetchRequest(entityName: entity)
+        request.returnsObjectsAsFaults = false;
+        
+        request.includesPropertyValues = false
+        
+        var results:[NSManagedObject] = context.executeFetchRequest(request, error: nil) as! Array
+        
+        for res in results{
+            context.deleteObject(res)
+        }
+        var err:NSError?
+        if !context.save(&err){
+            println(err)
+        }
+        else{
+            println("clear succeeded")
+        }
+        
+    }
+    
+    
+    @IBAction func loadData(sender: UIButton) {
+        
+        var appDel:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        var context:NSManagedObjectContext = appDel.managedObjectContext!
+        
+        var request = NSFetchRequest(entityName: "App")
+        request.returnsObjectsAsFaults = false;
+        
+        var errorFet:NSError?
+        
+        var results:Array = context.executeFetchRequest(request, error: &errorFet)!
+        
+        println(errorFet)
+        
+        for res in results {
+            println(res)
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
